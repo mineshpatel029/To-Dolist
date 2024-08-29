d16773357e2bd7ca3ea62b98b863f8c3e0adccd0
Minesh Patel <mineshpatel029@gmail.com>
2024-08-29 19:01:19 +0530

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CdkDrag, CdkDragDrop, CdkDropList, CdkDropListGroup, DragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+// import { RouteConfigLoadEnd } from '@angular/router';
+// import { Router } from 'express';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports:[CdkDropList,CdkDrag,CdkDropListGroup,FormsModule],
+  standalone:true,
 })
 export class AppComponent {
-  title = 'TD-list';
+  newTask: string = '';
+  todo: string[] = [];
+  done: string[] = [];
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.todo.push(this.newTask.trim());
+      this.newTask = '';
+    }
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
+  trackByFn(index: number, item: any): number {
+    return index;
+  }
 }
